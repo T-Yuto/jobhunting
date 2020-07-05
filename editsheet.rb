c462cc71068f21337a229510110219dacbf81e49
T-Yuto <u10.tk_tf.onnt@icloud.com>
2020-07-05 17:38:33 +0900

    add edit_phase

--- editsheet.rb
+++ editsheet.rb
@@ -1,20 +1,20 @@
+# encoding: utf-8
 require "google_drive"
+require "date"
 # Creates a session. This will prompt the credential via command line for the
 # first time and save it to config.json file for later usages.
 # See this document to learn how to create config.json:
 # https://github.com/gimite/google-drive-ruby/blob/master/doc/authorization.md
-
 session = GoogleDrive::Session.from_config("config.json")
 # https://docs.google.com/spreadsheets/d/1lmDJb49PxtkW-YHwjZpcovNpHsPybV9X8dhbgt8qnyM/edit#gid=1035003115
 ##スプレッドシート取得
 s_sheets = session.spreadsheet_by_url("https://docs.google.com/spreadsheets/d/1lmDJb49PxtkW-YHwjZpcovNpHsPybV9X8dhbgt8qnyM/edit#gid=1035003115")
 ##ワークシート取得
-w_sheet = s_sheets.worksheets[0]
+w_sheet = s_sheets.worksheets[4]
 # w_sheet[行, 列]で指定！！[row, column]  行: 番号、列: 英字
-##########
 
+## 入力済みデータ取得
 def set_data(w_sheet)
-  ## 入力済みデータ取得
   row = 1
   entered_data = []
   while true
@@ -30,35 +30,28 @@ def set_entered_data(row, w_sheet)
   return line
 end
 
-def list_i_name(i, name)
-  puts "------------------------------"
-  puts "#{i}  #{name}"
+def set_phase
+  return ["内定", "最終選考", "四次選考", "三次選考", "二次選考", "一次選考", "書類選考", "お見送り"]
 end
 
-# def sort_phase(data)
-#   phase = data[5]
-#   case phase
-#   when "お見送り"
-#   when "書類選考"
-#   when "一次選考"
-#   when "二次選考"
-#   when "三次選考"
-#   when "四次選考"
-#   when "最終選考"
-#   when "内定"
-# end
+## ここから処理記述
+def list_i_name_phase(i, name, phase)
+  puts "------------------------------------------------------------"
+  puts "#{i}:  #{name}  --  #{phase}"
+end
 
 def start(entered_data)
-  i = menu
-  link_menu(i, entered_data)
+  while link_menu(menu, entered_data) != 0
+  end
 end
 
 def menu
   puts "----- what's doing? -----"
-  puts "0  display entered list"
-  puts "1  add compony"
-  puts "2  edit jobsheet"
-  puts "3  sort menu"
+  puts "0  exit"
+  puts "1  display entered list"
+  puts "2  add compony"
+  puts "3  edit jobsheet"
+  puts "4  sort menu"
   puts "------------------------------"
   gets.to_i
 end
@@ -66,13 +59,21 @@ end
 def link_menu(num, entered_data)
   case num
   when 0
-    list_display(entered_data)
+    # 終了 exit
+    return 0
   when 1
-    add_compony(entered_data)
+    list_display(entered_data)
   when 2
-    edit_sheet(entered_data)
+    new_application = add_company(entered_data)
+    entered_data << new_application
   when 3
-    sort_menu(entered_data)
+    list_display(entered_data)
+    edit_sheet(entered_data, select_number + 12)
+  when 4
+    sort_data = entered_data[12, entered_data.length - 1]
+    sort_data.map!.with_index { |data, i| change_phase_number(data, i) }
+    sort_menu(sort_data)
+    sort_data.map!.with_index { |data, i| change_number_phase(data, i) }
   end
 end
 
@@ -80,16 +81,29 @@ def list_display(entered_data)
   puts "#########################"
   puts "----- list company  -----"
   puts "#########################"
-  puts entered_data.map.with_index { |data, i| list_i_name(i - 12, data[1]) if i >= 12 }
-  start(entered_data)
+  puts entered_data.map.with_index { |data, i|
+    return start(entered_data) if i >= 12 && data[1] == ""
+    list_i_name_phase(i - 12, data[1], data[4]) if i >= 12 && data[1] != ""
+  }
 end
 
-def add_company
+def add_company(entered_data)
   puts "------------------------------"
   puts "Please input compony name."
   compony_name = gets.chomp
   puts "------------------------------"
   puts "compony name: #{compony_name}"
+  puts "------------------------------"
+  puts "Please input compony place."
+  company_place = gets.chomp
+  puts "------------------------------"
+  puts "compony place: #{company_place}"
+  puts "------------------------------"
+  puts "Please input what's route."
+  application_route = gets.chomp
+  puts "------------------------------"
+  puts "application route: #{application_route}"
+  add_company_ary = ["", compony_name, company_place, application_route, "書類選考", Date.today, "", "", "", "", "", ""]
 end
 
 def select_preview(entered_data)
@@ -109,5 +123,129 @@ def phase_puts(entered_data)
   }
 end
 
+def select_number
+  puts "------------------------------"
+  puts "select input number"
+  puts "------------------------------"
+  gets.to_i
+end
+
+def sort_phase(entered_data)
+  entered_data.sort! { |a, b| a[5] <=> b[5] }
+end
+
+def change_phase_number(data, i)
+  current_phase = data[i][5]
+  # phase_array = set_phase
+  # phase_array.map.with_index{|phase, phase_i| data[i][5] = phase_i if phase == current_phase}
+  case current_phase
+  when "お見送り"
+    data[i][5] = 7
+  when "書類選考"
+    data[i][5] = 6
+  when "一次選考"
+    data[i][5] = 5
+  when "二次選考"
+    data[i][5] = 4
+  when "三次選考"
+    data[i][5] = 3
+  when "四次選考"
+    data[i][5] = 2
+  when "最終選考"
+    data[i][5] = 1
+  when "内定"
+    data[i][5] = 0
+  end
+end
+
+def change_number_phase(data, i)
+  current_phase = data[i][5]
+  # phase_array = set_phase
+  # phase_array.map.with_index{|phase, phase_i| data[i][5] = phase if phase_i == phase}
+  case phase
+  when 7
+    data[i][5] = "お見送り"
+  when 6
+    data[i][5] = "書類選考"
+  when 5
+    data[i][5] = "一次選考"
+  when 4
+    data[i][5] = "二次選考"
+  when 3
+    data[i][5] = "三次選考"
+  when 2
+    data[i][5] = "四次選考"
+  when 1
+    data[i][5] = "最終選考"
+  when 0
+    data[i][5] = "内定"
+  end
+end
+
+def edit_sheet(entered_data, company_i)
+  puts "------------------------------"
+  puts entered_data[company_i].join(" | ")
+  puts "what's company edit?"
+  entered_data[8].map.with_index { |column, i| puts "#{i} : #{column}" }
+  puts "------------------------------"
+  num = select_number
+  entered_data[company_i][num] = edit_company(entered_data, company_i, num)
+end
+
+def edit_company(entered_data, company_i, num)
+  case num
+  when 0
+    # aspirations(entered_data)
+  when 1
+    # edit_name(entered_data, company_i)
+  when 2
+    # edit_place(entered_data, company_i)
+  when 3
+    # edit_application_route(entered_data, company_i)
+  when 4
+    return edit_phase(entered_data, company_i)
+  when 5
+    # edit_application_day(entered_data, company_i)
+  when 6
+    # edit_primary_selection_day(entered_data, company_i)
+  when 7
+    # edit_second_selection_day(entered_data, company_i)
+  when 8
+    # edit_final_selection_day(entered_data, company_i)
+  when 9
+    # edit_charm_point(entered_data, company_i)
+  when 10
+    # edit_concern_point(entered_data, company_i)
+  when 11
+    # edit_reflection_point(entered_data, company_i)
+  when 12
+    # edit_feedback(entered_data, company_i)
+  when 13
+    # edit_remarks(entered_data, company_i)
+  end
+  unimplemented
+end
+
+def edit_phase(entered_data, company_i)
+  phase_array = set_phase
+  puts "------------------------------"
+  phase_array.map.with_index { |phase, i| puts "#{i} : #{phase}" }
+  puts "------------------------------"
+  puts "\n#{entered_data[company_i][1]} : #{entered_data[company_i][4]}"
+  puts "どの選考フェーズか、番号で入力してください。"
+  entered_data[company_i][4] = phase_array[gets.to_i]
+end
+
+def def(unimplemented)
+  puts "------------------------------"
+  puts "未実装です。"
+  puts "------------------------------"
+end
+
 entered_data = set_data(w_sheet)
 start(entered_data)
+
+# entered_data.map.with_index { |data, i|
+#   puts data.join("  ") if i == 8
+#   puts data.join(" | ") if i >= 12 && data[1] != ""
+# }
